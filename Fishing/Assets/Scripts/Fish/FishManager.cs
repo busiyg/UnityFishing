4268cd5d5079fa6896bb132018d7d7bb6d50a282
DESKTOP-OSHLUMQ\yao <busiyg@163.com>
2018-07-13 16:32:43 +0800

    Update

---
+++ Fishing/Assets/Scripts/Fish/FishManager.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishManager : MonoBehaviour {
+    private static FishManager Instance;
+    public List<FishInfoModel> FishModels;
+    public GameObject FishPrefab;
+    public List<FishTeamController> FishTeams;
+    public List<LittleTeamController> LittleTeams;
+
+    public float Timer;
+    public float interval;
+
+    public static FishManager GetInstance() {
+        return Instance;
+    }
+
+    private void Awake() {
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Timer += Time.deltaTime;
+        if (Timer > interval) {
+            TestInitFish();
+            Timer = 0;
+        }
+
+    }
+
+    public GameObject CreatFishByID(int ID) {
+        foreach (var obj in FishModels) {
+            if (ID==obj.fishType) {
+                var fish = Instantiate(FishPrefab);
+                fish.GetComponent<FishController>().InitFish(obj);
+                return fish;
+                break;
+            }
+        }
+        return null;
+    }
+
+    public GameObject ReturnLittleTeamByID(int ID) {
+        foreach (var obj in LittleTeams) {
+            if (ID==obj.ID) {
+                return obj.gameObject;
+            }
+        }
+        return null;
+    }
+
+    public void InitFishTeam(int teamID,List<int> fishs) {
+        foreach (var obj in FishTeams) {
+            if (obj.TeamID== teamID) {
+                var team = Instantiate(obj.gameObject);
+                team.GetComponent<FishTeamController>().InitFishs(fishs);
+                break;
+            }
+        }
+    }
+
+    public void InitLittleTeam(int ID,int FishID,int PathID) {
+        var littleTeam = Instantiate(ReturnLittleTeamByID(ID)); 
+      
+        var thePath = PathManager.GetInstance().ReturnPath(PathID);
+        littleTeam.GetComponent<LittleTeamController>().InitFish(FishID);
+        littleTeam.transform.position = thePath[0];
+        littleTeam.GetComponent<LittleTeamController>().MoveByPath(thePath);
+    }
+
+
+
+    public void InitFish(int PathID,int FishID) {
+        var thePath = PathManager.GetInstance().ReturnPath(PathID);
+        var theFish = CreatFishByID(FishID);
+        theFish.transform.position = thePath[0];
+        theFish.GetComponent<FishController>().MoveByPath(thePath);
+    }
+
+    public void TestInitTeam(int id) {
+        List<int> fishs = new List<int>() { 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+        InitFishTeam(id, fishs);
+    }
+
+    public void TestInitFish() {
+        var RandomPath =Random.Range(0, 3);
+        var RandomFish = Random.Range(0, FishModels.Count);
+        InitFish(RandomPath,RandomFish);
+    }
+
+    public void TestLittleTeam() {
+        var RandomLittleTeam = Random.Range(0, 2);
+        var RandomFish = Random.Range(0, FishModels.Count);
+        var RandomPath = Random.Range(0, 3);
+        InitLittleTeam(RandomLittleTeam,RandomFish,RandomPath);
+    }
+}
---
+++ Fishing/Assets/Scripts/LittleTeamController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class LittleTeamController : MonoBehaviour {
+    public float Speed;
+    public int ID;
+    public List<Transform> Trans;
+    public List<GameObject> Fishes;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MoveByPath(List<Vector3> transList) {
+        var trans = transList.ToArray();
+        gameObject.transform.DOLocalPath(trans, 1/Speed, PathType.CatmullRom, PathMode.Sidescroller2D, 1).SetLookAt(0.01f).OnComplete(() => {
+            Destroy(gameObject);
+        });
+    }
+
+    public void InitFish(int FishID) {
+        var fishManager = FishManager.GetInstance();
+        for (int i=0;i< Trans.Count;i++) {
+            var fish = fishManager.CreatFishByID(FishID);
+            fish.transform.position = Trans[i].position;
+            fish.transform.rotation = Trans[i].rotation;
+            fish.transform.SetParent(gameObject.transform);
+            Fishes.Add(fish);
+        }
+    }
+}
